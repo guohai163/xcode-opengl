53f4c315497e721750ec6b59bb6c68159f6d8c4d
郭海 <guohai@guohaideMac-mini.local>
2017-12-21 20:23:42 +0800

    着色器已经实现，目前实现变色中，有BUG

--- opengldemo/opengldemo/main.cpp
+++ opengldemo/opengldemo/main.cpp
@@ -7,15 +7,35 @@
 //
 
 #include <iostream>
+#include <cmath>
 //#define GLFW_INCLUDE_GLCOREARB
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+void someOpenGLFunctionThatDrawOurTriangle(unsigned int shaderProgram, unsigned int VAO);
+
+//顶点着色器 GLSL
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+//片段着色器
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"uniform vec4 ourColor;\n"
+"void main()\n"
+"{\n"
+"    FragColor = ourColor;\n"
+"} ";
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+
+
     if (!glfwInit())
     {
         return -1;
@@ -27,6 +47,9 @@ int main(int argc, const char * argv[]) {
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
 #endif
+    
+    
+
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -45,6 +68,98 @@ int main(int argc, const char * argv[]) {
         return -1;
     }
     
+    //第二课，你好三角型
+    
+    //着色器 unifrom
+
+    
+    
+    //创建着色器
+    unsigned int vertexShader,fragmentShader,shaderProgram;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1 , &vertexShaderSource, NULL);
+    //动态编辑着色器
+    glCompileShader(vertexShader);
+    //检查编译器是否成功
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    //创建片段着色 器
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1 ,&fragmentShaderSource,NULL);
+    glCompileShader(fragmentShader);
+    //检查
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    //加载着色器
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram,vertexShader);
+    glAttachShader(shaderProgram,fragmentShader);
+    glLinkProgram(shaderProgram);
+    //检查加载着色器是否成功
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::load shader program\n" << infoLog << std::endl;
+    }
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    //三角顶点坐标
+    float vertices[] = {
+        0.0f,0.0f,0.0f,
+        0.5f,0.5f,0.0f,
+        0.5f,-0.5f,0.0f,
+        0.0f,0.0f,0.0f,
+        -0.5f,0.5f,0.0f,
+        -0.5f,-0.5f,0.0f
+        
+        
+    };
+    //VBO  Vertex Array Object 顶点数组对象
+    unsigned int VBO,VAO;
+    //创建顶点数组
+    glGenVertexArrays(1, &VAO);
+    //生成VBO对象
+    glGenBuffers(1, &VBO);
+    
+    //绑定
+    glBindVertexArray(VAO);
+    
+    //绑定缓存
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //用户定义的数据复制到当前绑定缓冲
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    
+    
+    //// 1. 设置顶点属性指针
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+    
+    
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -53,12 +168,15 @@ int main(int argc, const char * argv[]) {
         // -----
         processInput(window);
         
+        someOpenGLFunctionThatDrawOurTriangle(shaderProgram, VAO);
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
     glfwTerminate();
@@ -77,7 +195,31 @@ void processInput(GLFWwindow *window)
 // ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+    std::cout << "window size change" << width <<"\t" << height << std::endl;
     // make sure the viewport matches the new window dimensions; note that width and
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+
+//绘制物体
+void someOpenGLFunctionThatDrawOurTriangle(unsigned int shaderProgram, unsigned int VAO)
+{
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    //图形渲染部分
+    glUseProgram(shaderProgram);
+    
+    
+    float timeValue = glfwGetTime();
+    float greenValue = (sin(timeValue)/2.0f)+0.5f;
+    int vertexColorLocation = glGetUniformLocation(shaderProgram,"ourColor");
+
+    std::cout << "greenvalue\t" <<greenValue <<std::endl;
+
+    
+    
+    glUniform4f(vertexColorLocation,0.0f, greenValue,0.0f,1.0f);
+    
+    //glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+    glDrawArrays(GL_TRIANGLES, 0, 5);
 }
