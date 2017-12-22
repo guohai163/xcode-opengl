242dd44d990f1b37d3ae7c785ef6362c232b2b8f
郭海 <guohai@guohaideMac-mini.local>
2017-12-22 12:11:05 +0800

    本次实现从顶点着色器，输入顶点颜色 在片段着色器进行颜色渲染

--- opengldemo/opengldemo/main.cpp
+++ opengldemo/opengldemo/main.cpp
@@ -16,21 +16,31 @@ void processInput(GLFWwindow *window);
 void someOpenGLFunctionThatDrawOurTriangle(unsigned int shaderProgram, unsigned int VAO);
 
 //顶点着色器 GLSL
+//const char *vertexShaderSource = "#version 330 core\n"
+//"layout (location = 0) in vec3 aPos;\n"
+//"void main()\n"
+//"{\n"
+//"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+//"}\0";
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   ourColor = aColor;"
 "}\0";
+
 
 //片段着色器
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"    FragColor = ourColor;\n"
-"} ";
+"    FragColor = vec4(ourColor, 1.0);\n"
+"}\0";
 
 int main(int argc, const char * argv[]) {
 
@@ -122,12 +132,10 @@ int main(int argc, const char * argv[]) {
     // ------------------------------------------------------------------
     //三角顶点坐标
     float vertices[] = {
-        0.0f,0.0f,0.0f,
-        0.5f,0.5f,0.0f,
-        0.5f,-0.5f,0.0f,
-        0.0f,0.0f,0.0f,
-        -0.5f,0.5f,0.0f,
-        -0.5f,-0.5f,0.0f
+        // 位置              // 颜色
+        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
         
         
     };
@@ -149,14 +157,23 @@ int main(int argc, const char * argv[]) {
     
     
     //// 1. 设置顶点属性指针
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
+//    第一个参数指定我们要配置的顶点属性。还记得我们在顶点着色器中使用layout(location = 0)定义了position顶点属性的位置值(Location)吗？它可以把顶点属性的位置值设置为0。因为我们希望把数据传递到这一个顶点属性中，所以这里我们传入0。
+//    第二个参数指定顶点属性的大小。顶点属性是一个vec3，它由3个值组成，所以大小是3。
+//    第三个参数指定数据的类型，这里是GL_FLOAT(GLSL中vec*都是由浮点数值组成的)。
+//    下个参数定义我们是否希望数据被标准化(Normalize)。如果我们设置为GL_TRUE，所有数据都会被映射到0（对于有符号型signed数据是-1）到1之间。我们把它设置为GL_FALSE。
+//    第五个参数叫做步长(Stride)，它告诉我们在连续的顶点属性组之间的间隔。由于下个组位置数据在3个float之后，我们把步长设置为3 * sizeof(float)。要注意的是由于我们知道这个数组是紧密排列的（在两个顶点属性之间没有空隙）我们也可以设置为0来让OpenGL决定具体步长是多少（只有当数值是紧密排列时才可用）。一旦我们有更多的顶点属性，我们就必须更小心地定义每个顶点属性之间的间隔，我们在后面会看到更多的例子（译注: 这个参数的意思简单说就是从这个属性第二次出现的地方到整个数组0位置之间有多少字节）。
+//    最后一个参数的类型是void*，所以需要我们进行这个奇怪的强制类型转换。它表示位置数据在缓冲中起始位置的偏移量(Offset)。由于位置数据在数组的开头，所以这里是0。我们会在后面详细解释这个参数。
     
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    //glBindBuffer(GL_ARRAY_BUFFER, 0);
     
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
+    //glBindVertexArray(0);
+    glBindVertexArray(VAO);
 
     
     
@@ -210,16 +227,16 @@ void someOpenGLFunctionThatDrawOurTriangle(unsigned int shaderProgram, unsigned 
     glUseProgram(shaderProgram);
     
     
-    float timeValue = glfwGetTime();
-    float greenValue = (sin(timeValue)/2.0f)+0.5f;
-    int vertexColorLocation = glGetUniformLocation(shaderProgram,"ourColor");
-
-    std::cout << "greenvalue\t" <<greenValue <<std::endl;
+//    float timeValue = glfwGetTime();
+//    float greenValue = (sin(timeValue)/2.0f)+0.5f;
+//    int vertexColorLocation = glGetUniformLocation(shaderProgram,"ourColor");
+//
+//    std::cout << "greenvalue\t" <<greenValue <<std::endl;
 
     
     
-    glUniform4f(vertexColorLocation,0.0f, greenValue,0.0f,1.0f);
+//    glUniform4f(vertexColorLocation,0.0f, greenValue,0.0f,1.0f);
     
     //glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-    glDrawArrays(GL_TRIANGLES, 0, 5);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
